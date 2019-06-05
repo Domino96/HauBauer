cb10a9092b291a0952ffe11fc04be7c276bc3872
ttriantafyllou <tt@digital-control.biz>
2019-06-05 11:50:00 +0200

    combobox list items added

--- src/de/haubauer/ui/controllers/rentalproperties/PaymentsEditController.java
+++ src/de/haubauer/ui/controllers/rentalproperties/PaymentsEditController.java
@@ -6,6 +6,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -30,16 +39,21 @@ public class PaymentsEditController implements Initializable {
 
     //ComboBox
     @FXML
-    private ComboBox payEPaymentTypeComboBox;
+    private ComboBox<String> payEPaymentTypeComboBox;
 
     //Label
     @FXML
     private Label payEIdLabel;
 
+    ObservableList<String> list = FXCollections.observableArrayList();
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        list.add("Hello");
+        list.add("New");
+        list.add("World");
 
+        payEPaymentTypeComboBox.setItems(list);
     }
 }
