224cf789f497e99d7c51264469c6f43822fc222f
Christopher Engels <christopherengels.de@googlemail.com>
2019-06-07 10:44:08 +0200

    Add FXML annotated elements

--- src/de/haubauer/ui/controllers/tenants/TenantsDialogController.java
+++ src/de/haubauer/ui/controllers/tenants/TenantsDialogController.java
@@ -3,6 +3,7 @@ package src.de.haubauer.ui.controllers.tenants;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import src.de.haubauer.business.services.PersonService;
 import src.de.haubauer.ui.viewmodels.TenantsDialogViewModel;
 
@@ -15,6 +16,42 @@ public abstract class TenantsDialogController implements Initializable {
 
     @FXML
     private Label title;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField streetField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField houseNumberField;
+
+    @FXML
+    private TextField zipCodeField;
+
+    @FXML
+    private TextField townField;
+
+    @FXML
+    private TextField ibanField;
+
+    @FXML
+    private TextField bicField;
+
+    @FXML
+    private TextField accountOwnerField;
+
+    @FXML
+    private TextField bankField;
 
     public TenantsDialogController(String title) {
         this.viewModel.setTitle(title);
