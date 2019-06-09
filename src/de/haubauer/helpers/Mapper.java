2d64155f0fbe0888d0fce8e2668f2288497fc12e
Christopher Engels <christopherengels.de@googlemail.com>
2019-06-09 15:58:10 +0200

    Add list overload to Mapper

--- src/de/haubauer/helpers/Mapper.java
+++ src/de/haubauer/helpers/Mapper.java
@@ -2,6 +2,8 @@ package de.haubauer.helpers;
 
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+
 /**
  * Ein Wrapper für ModelMapper, um das Mapping zwischen Entity (Datenschicht) und Model (Logikschicht) zu vereinfachen.
  */
@@ -29,5 +31,17 @@ public class Mapper {
      */
     public <TSource, TDestination> TDestination map(TSource source, Class<TDestination> destinationClass) {
         return this.modelMapper.map(source, destinationClass);
+    }
+
+    /**
+     * Mappt die angegebenen Quellobjekte zur angegebenen Klasse.
+     * @param source Eine Liste von Quellobjekten.
+     * @param destinationClass Die Klasse, in das das Quellobjekt gemappt werden soll.
+     * @param <TSource> Den Typ des Quellobjektes.
+     * @param <TDestination> Den Typ des Zielobjektes.
+     * @return Eine Liste von TDestination, das die Zielobjekte enthält.
+     */
+    public <TSource, TDestination> List<TDestination> map(List<TSource> source, Class<TDestination> destinationClass) {
+        return this.modelMapper.<List<TDestination>>map(source, destinationClass);
     }
 }
